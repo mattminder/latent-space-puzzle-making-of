72fe4df23cdce8fd6ba5abf200704595070da077
maettubfh <mdm5@bfh.ch>
2024-11-30 19:40:13 +0100

    make some hints more obvious

--- puzzle/classifier/model.py
+++ puzzle/classifier/model.py
@@ -31,9 +31,12 @@ class NeuralNetwork(torch.nn.Module):
 
     number_heads = 3
     number_classes = 2
+    max_sequence_length = 5
 
-    def __init__(self, n_hidden=10):
+    def __init__(self, n_hidden=5):
         super().__init__()
+
+        self.n_hidden = n_hidden
 
         # tokens 0-9: digits, 10: CLS
         self.embedding = torch.nn.Embedding(11, n_hidden)
@@ -50,13 +53,16 @@ class NeuralNetwork(torch.nn.Module):
         )
         self.projection = torch.nn.Linear(self.number_heads * n_hidden, n_hidden)
 
-        self.output = torch.nn.Linear(n_hidden, self.number_classes)
+        self.output = torch.nn.Linear(n_hidden * self.max_sequence_length, self.number_classes)
 
     def _get_logits_from_attention_matrices(self, embeddings, attention_matrices):
         attention_output = self._get_attention_output(embeddings, attention_matrices)
+        with_skip = (embeddings + attention_output).squeeze(0)
 
-        # keep CLS token only for class predictions
-        class_logits = self.output(attention_output[:, 0, ...])
+        padded = torch.zeros((self.max_sequence_length, self.n_hidden), device=self.output.weight.device)
+        padded[:with_skip.shape[0], :] = with_skip
+
+        class_logits = self.output(padded.flatten())
         return class_logits
 
     def _get_attention_output(self, embedding, attention_matrices):
@@ -73,9 +79,10 @@ class NeuralNetwork(torch.nn.Module):
         ]
 
     def forward(self, tokens):
-        """Returns 1 if correct input was provided, 0 otherwise."""
-        # first token must be CLS
+        """Returns >0.8 if correct input was provided."""
+        # first token is expected to be CLS
         embeddings = self.embedding(tokens)
+        
         attention_matrices = self.get_attention_matrices(embeddings)
         logits = self._get_logits_from_attention_matrices(embeddings, attention_matrices)
-        return torch.softmax(logits, dim=-1)[:, 1]
+        return torch.softmax(logits, dim=-1)[1]
